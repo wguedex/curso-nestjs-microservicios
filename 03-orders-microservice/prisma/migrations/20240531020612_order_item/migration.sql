24a8dad60eae7f57a507b21f3df444d0903aa81c
Wilmer Guedez <w.guedex@gmail.com>
2024-05-30 21:08:01 -0500

    03-orders-microservice: OrderItems - Detalles de la orden

---
+++ 03-orders-microservice/prisma/migrations/20240531020612_order_item/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "stripeChargeId" TEXT,
+ALTER COLUMN "status" SET DEFAULT 'PENDING';
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "orderId" TEXT,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE SET NULL ON UPDATE CASCADE;
