4d010f5ad8fd3d62707824159b59320a0b20043b
Wilmer Guedez <w.guedex@gmail.com>
2024-06-02 14:08:30 -0500

    03-orders-microservice: NatsModule - Modulo personalizado

--- 02-client-gateway/src/products/products.module.ts
+++ 02-client-gateway/src/products/products.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common'; 
-import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs } from 'src/config';
-import { NATS_SERVICE } from 'src/config/services';
+import { ProductsController } from './products.controller'; 
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports: [
-    ClientsModule.register([
-      { name: NATS_SERVICE, 
-        transport: Transport.NATS, 
-        options: {
-          servers: envs.natsServers, 
-        } },
-    ]),
+    NatsModule
   ]
 
 })
