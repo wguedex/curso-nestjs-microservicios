c1bf7b98921fd053b54e3d307b9222791f145957
Wilmer Guedez <148584989+wguedex@users.noreply.github.com>
2024-06-03 18:16:02 -0500

    pagos: payments - microservice: configuracion de stripe

--- 04-payments-microservice/src/config/envs.ts
+++ 04-payments-microservice/src/config/envs.ts
@@ -3,11 +3,13 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;  
+  STRIPE_SECRET: string;
 }
 
 const envsSchema = joi
   .object({
-    PORT: joi.number().required()   
+    PORT: joi.number().required(), 
+    STRIPE_SECRET: joi.string().required()
   })
   .unknown(true);
 
@@ -20,5 +22,6 @@ if (error) {
 const envVars: EnvVars = value;
  
 export const envs = { 
-    port: envVars.PORT 
+    port: envVars.PORT, 
+    stripeSecret: envVars.STRIPE_SECRET
 };
--- 04-payments-microservice/src/payments/payments.service.ts
+++ 04-payments-microservice/src/payments/payments.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+
+    private readonly stripe = new Stripe(envs.stripeSecret)
+
+    
+
+}
