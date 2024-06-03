785eca62a00ab3d9a7012b2abe57fff6c21f6512
Wilmer Guedez <w.guedex@gmail.com>
2024-06-02 19:00:23 -0500

    docker - crear docker compose: crear red y levantar todo con un solo comando

--- 02-client-gateway/src/main.ts
+++ 02-client-gateway/src/main.ts
@@ -6,7 +6,7 @@ import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
   const logger = new Logger('Main Gateway');
-
+ 
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
